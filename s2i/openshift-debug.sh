ad84201b9d3cf113800b7033fbdecd9158cf34cc
Mike Echevarria <mechevar@redhat.com>
2018-09-12 17:06:09 -0400

    refactoring and cleanup of scripts

--- s2i/openshift-debug.sh
+++ s2i/openshift-debug.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# https://docs.openshift.com/container-platform/3.10/dev_guide/port_forwarding.html
+
 pod_name=$(oc get pods --selector app=jws-s2i | { read line1 ; read line2 ; echo "$line2" ; } | awk '{print $1;}')
 
 echo "Setup remote debugging in your IDE with localhost and port 8000"
