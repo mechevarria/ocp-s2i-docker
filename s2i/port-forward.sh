beffb96ddee71c1951d439e342b69444c56017e0
Mike Echevarria <mechevar@redhat.com>
2018-09-12 13:51:50 -0400

    refactor

--- s2i/port-forward.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-pod_name=$(oc get pods --selector app=jws-s2i | { read line1 ; read line2 ; echo "$line2" ; } | awk '{print $1;}')
-
-echo "Setup remote debugging in your IDE with localhost and port 8000"
-oc port-forward ${pod_name} 8000:8000
