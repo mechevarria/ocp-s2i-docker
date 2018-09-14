1b9b24f0f2473be1dd08e67eeb2f3cd37596c9d4
Mike Echevarria <mechevar@redhat.com>
2018-09-14 14:14:06 -0400

    cleanup echo statements, add comments

--- s2i/openshift-hotdeploy.sh
+++ s2i/openshift-hotdeploy.sh
@@ -3,12 +3,22 @@
 # https://docs.okd.io/latest/dev_guide/copy_files_to_container.html
 
 pod_name=$(oc get pods --selector app=jws-s2i | { read line1 ; read line2 ; echo "$line2" ; } | awk '{print $1;}')
-echo "pod_name=${pod_name}"
 
-deploy_dir=/opt/jws-5.0/tomcat/webapps/ROOT
-echo "deploy_dir=${deploy_dir}"
+# directories on pod
+root_dir=/opt/jws-5.0/tomcat/webapps/ROOT
+classes_dir=${root_dir}/WEB-INF
 
-dev_dir=$(pwd)/target/jws-app/
-echo "dev_dir=${dev_dir}"
+# local build directories
+compile_dir=$(pwd)/target/classes
+web_dir=$(pwd)/src/main/webapp/
 
-oc rsync ${dev_dir} ${pod_name}:${deploy_dir} --watch --delete
+# sync java classes and html in parallel since they are in different directories
+oc rsync ${compile_dir} ${pod_name}:${classes_dir} --watch & \
+java_pid=$!
+oc rsync ${web_dir} ${pod_name}:${root_dir} --watch & \
+html_pid=$!
+
+sleep 1
+read -p "Press any key to exit..."  -n1 -s
+kill ${java_pid}
+kill ${html_pid}
