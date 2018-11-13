ac7988c30e3c4359cfc174c46729c164a37d15a1
Mike Echevarria <mechevar@redhat.com>
2018-11-13 12:47:20 -0500

    remove unused builder and simplify assemble script

--- builder/4-docker-run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-port=8080
-registry=$(minishift openshift registry)
-project=myproject
-app=tomcat9-app
-tag=${registry}/${project}/${app}:latest
-
-docker run -p ${port}:${port} ${tag}
