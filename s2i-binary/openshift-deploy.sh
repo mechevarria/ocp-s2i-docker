c517e240e6bc5041a1e99d0a2f3636ec17f655fa
Mike Echevarria <mechevar@redhat.com>
2018-11-13 15:12:17 -0500

    refactor for binary build

---
+++ s2i-binary/openshift-deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# prefixing project with user to allow multiple people building the same project on the same cluster
+proj_name="$(oc whoami)-eap-build"
+proj_exists="$(oc projects | grep ${proj_name})"
+
+# if project doesn't exist, make a new one.  Otherwise switch to that project
+if [[ -z ${proj_exists} ]]; then
+  oc new-project ${proj_name} --display-name="EAP Build Examples" --description="Examples of s2i, s2i-binary and docker builds with the EAP Openshift image"
+else
+  oc project ${proj_name}
+fi
+
+oc new-app \
+--name=eap-s2i \
+-p SOURCE_REPOSITORY_URL=https://github.com/mechevarria/ocp-s2i-docker \
+-p SOURCE_REPOSITORY_REF=eap \
+-p CONTEXT_DIR=/s2i \
+--template=eap71-basic-s2i
--- s2i/openshift-hotdeploy.sh
+++ s2i/openshift-hotdeploy.sh
@@ -5,9 +5,9 @@
 pod_name=$(oc get pods --selector app=eap-s2i | { read line1 ; read line2 ; echo "$line2" ; } | awk '{print $1;}')
 
 # directory on pod
-deploy_dir=/deployments/ROOT
+deploy_dir=/deployments/
 
-# exploded war directory
-exploded_dir=$(pwd)/target/s2i-1.0-SNAPSHOT/
+# war directory
+exploded_dir=$(pwd)/deployments/
 
 oc rsync ${exploded_dir} ${pod_name}:${deploy_dir} --watch
